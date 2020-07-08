70b3b985d7c8efc5d184707745bcde4ecd356141
Jean-Luc <thefajeth@gmail.com>
2020-07-08 22:53:35 +0200

    Freeze gefixt. Endlosschleife gefixt.

--- Scripts/Measures/ms_211_OrderCollectEvidence.lua
+++ Scripts/Measures/ms_211_OrderCollectEvidence.lua
@@ -1,27 +1,22 @@
 function Run()
 	--how far the Destination can be to start this action
-	local MaxDistance = 500
-	--how far from the destination, the owner should stand while reading the letter from rome
+	local MaxDistance = 1000
+	--how far from the destination, the owner should stand
 	local ActionDistance = 80
-
-	--StartGameTimer(8)
-	MeasureSetStopMode(STOP_NOMOVE)
 	
-	f_ExitCurrentBuilding("Owner")
+	f_ExitCurrentBuilding("")
 	
-	if not AliasExists("Destination") then
-		CopyAlias("", "Destination")
-	end
-	MeasureSetNotRestartable() 
-	f_MoveTo("","Destination")
+	f_MoveTo("", "Destination")
 	
-	if not SimGetWorkingPlace("Owner","WorkBuilding") then
+	if not SimGetWorkingPlace("", "WorkBuilding") then
 		return
 	end
+
+	-- special case privilege
 	local IsMonitor = false
-	if not BuildingGetOwner("WorkBuilding","BuildingOwner") then
-		if SimGetProfession("")==GL_PROFESSION_MONITOR then
-			SimGetServantDynasty("","BuildingOwner")
+	if not BuildingGetOwner("WorkBuilding", "BuildingOwner") then
+		if SimGetProfession("") == GL_PROFESSION_MONITOR then
+			SimGetServantDynasty("", "BuildingOwner")
 			IsMonitor = true
 		--sim is monitor
 		else
@@ -29,18 +24,23 @@ function Run()
 		end
 	end
 
-	MsgMeasure("","@L_GENERAL_MEASURES_211_ORDERCOLLECTEVIDENCE_ACTION_+0")
+	-- show a msg below
+	MsgMeasure("", "@L_GENERAL_MEASURES_211_ORDERCOLLECTEVIDENCE_ACTION_+0")
 
 	local	Total = 0
 	
-	SetData("IsProductionMeasure", 0)
-	SimSetProduceItemID("", -GetCurrentMeasureID(""), -1)
-	SetData("IsProductionMeasure", 1)
-	
 	while true do
 		
-		local NumOfObjects = Find("Owner","__F( (Object.GetObjectsByRadius(Sim)==3000) AND NOT(Object.BelongsToMe())AND(Object.CanBeInterrupted(OrderCollectEvidence))AND NOT(Object.GetState(cutscene))AND NOT(Object.HasImpact(HasBeenTalked))AND NOT(Object.GetProfession() == 25)AND NOT(Object.GetProfession() == 21)AND NOT(Object.GetProfession() == 22))","Sims",-1)
-		if NumOfObjects>0 then
+		if not AliasExists("Destination") then
+			GetSettlement("", "City")
+			if CityFindCrowdedPlace("City", "", "Destination") == 0 then
+				break
+			end
+		end
+		
+		local NumOfObjects = Find("Owner","__F( (Object.GetObjectsByRadius(Sim)==1000) AND NOT(Object.BelongsToMe())AND(Object.CanBeInterrupted(OrderCollectEvidence))AND NOT(Object.GetState(cutscene))AND NOT(Object.HasImpact(HasBeenTalked))AND NOT(Object.GetProfession() == 25)AND NOT(Object.GetProfession() == 21)AND NOT(Object.GetProfession() == 22))","Sims",-1)
+		
+		if NumOfObjects > 0 then
 			local DestAlias = "Sims"..Rand(NumOfObjects)
 			
 			local Check = true
@@ -48,7 +48,7 @@ function Run()
 				Check = false
 			end
 			
-			if HasProperty(DestAlias,"QuestActive") then
+			if HasProperty(DestAlias, "QuestActive") then
 				Check = false
 			end
 		
@@ -56,44 +56,49 @@ function Run()
 			
 				SetData("Blocked", 0)
 				if ai_StartInteraction("", DestAlias, MaxDistance, ActionDistance, "BlockMe") then
-					AddImpact(DestAlias,"HasBeenTalked",1,6)
-					AlignTo(DestAlias,"")
+					AddImpact(DestAlias, "HasBeenTalked", 1, 6)
+					AlignTo(DestAlias, "")
 					Sleep(0.7)
 					PlayAnimationNoWait("Owner", "talk")
-					if SimGetGender("")==GL_GENDER_MALE then
-						PlaySound3DVariation("","CharacterFX/male_neutral",1)
+					if SimGetGender("") == GL_GENDER_MALE then
+						PlaySound3DVariation("", "CharacterFX/male_neutral", 1)
 					else
-						PlaySound3DVariation("","CharacterFX/female_neutral",1)
+						PlaySound3DVariation("", "CharacterFX/female_neutral", 1)
 					end
-					Sleep(0.75)
+					Sleep(1)
 					PlayAnimation(DestAlias, "talk")
 					local Cnt = Talk("", DestAlias, true)
-					if Cnt>0 then
+					if Cnt > 0 then
 						if SimGetGender("")==GL_GENDER_MALE then
-							PlaySound3DVariation("","CharacterFX/male_amazed",1)
+							PlaySound3DVariation("", "CharacterFX/male_amazed", 1)
 						else
-							PlaySound3DVariation("","CharacterFX/female_amazed",1)
+							PlaySound3DVariation("", "CharacterFX/female_amazed", 1)
 						end
+
 						if IsMonitor then
 							feedback_MessageCharacter("BuildingOwner",
 								"@L_GENERAL_MEASURES_211_ORDERCOLLECTEVIDENCE_MSG_SUCCESS_HEAD_+0",
-								"@L_GENERAL_MEASURES_211_ORDERCOLLECTEVIDENCE_MSG_SUCCESS_BODY_+0",GetID(""))
+								"@L_GENERAL_MEASURES_211_ORDERCOLLECTEVIDENCE_MSG_SUCCESS_BODY_+0", GetID(""))
 							Total = Total + Cnt
 						else
 							feedback_MessageCharacter("",
 								"@L_GENERAL_MEASURES_211_ORDERCOLLECTEVIDENCE_MSG_SUCCESS_HEAD_+0",
-								"@L_GENERAL_MEASURES_211_ORDERCOLLECTEVIDENCE_MSG_SUCCESS_BODY_+0",GetID(""))
+								"@L_GENERAL_MEASURES_211_ORDERCOLLECTEVIDENCE_MSG_SUCCESS_BODY_+0", GetID(""))
 							Total = Total + Cnt
+							IncrementXPQuiet("", 15)
 						end
 					end
 				else
-					f_MoveTo("","Destination",GL_MOVESPEED_RUN)
-					Sleep(1)
+					f_MoveTo("", "Destination",GL_MOVESPEED_RUN)
+					Sleep(2)
 				end
 				SetData("Blocked", 1)
 			end
 		else
-			f_MoveTo("","Destination")
+			if GetDistance("", "Destination") > 500 then
+				f_MoveTo("", "Destination")
+			end
+			Sleep(2)
 		end
 		Sleep(2)
 	end
@@ -112,12 +117,13 @@ function Run()
 	
 	MeasureRun("","Destination",2110)
 	
-	--f_MoveTo("","Workbuilding")
 end
 
 function BlockMe()
-	while GetData("Blocked")~=1 do
-		Sleep(1)
+	if HasData("Blocked") then
+		while GetData("Blocked") ~= 1 do
+			Sleep(3)
+		end
 	end
 end
 
