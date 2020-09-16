0731a7d06668f8b0583305a4ac4ec15482070479
Jean-Luc <thefajeth@gmail.com>
2020-09-16 12:19:54 +0200

    Wrong Itemname + Cleanup

--- Scripts/Measures/Artefacts/as_hpfz_Useoptigold.lua
+++ Scripts/Measures/Artefacts/as_hpfz_Useoptigold.lua
@@ -1,8 +1,8 @@
 function Run()
 
 	if IsStateDriven() then
-		local ItemName = "optigold"
-		if GetItemCount("", ItemName, INVENTORY_STD)==0 then
+		local ItemName = "Optigold"
+		if GetItemCount("", ItemName, INVENTORY_STD) == 0 then
 			if not ai_BuyItem("", ItemName, 1, INVENTORY_STD) then
 				return
 			end
@@ -14,39 +14,40 @@ function Run()
 
 	MeasureSetNotRestartable()
 	SetMeasureRepeat(TimeOut)	
-	local Time = PlayAnimationNoWait("","use_book_standing")
+	local Time = PlayAnimationNoWait("", "use_book_standing")
 	Sleep(1)
-	CarryObject("","Handheld_Device/ANIM_openscroll.nif",false)
-	PlaySound3D("","Locations/wear_clothes/wear_clothes+0.wav", 1.0)
+	CarryObject("", "Handheld_Device/ANIM_openscroll.nif", false)
+	PlaySound3D("", "Locations/wear_clothes/wear_clothes+0.wav", 1.0)
 	Sleep(Time-2)
-	CarryObject("","",false)
-	PlaySound3D("","Effects/coins_to_moneybag+0.wav", 1.0)
+	CarryObject("", "",false)
+	PlaySound3D("", "Effects/coins_to_moneybag+0.wav", 1.0)
 
-	RemoveItems("","optigold",1)
-	GetPosition("Owner", "ParticleSpawnPos")
-	StartSingleShotParticle("particles/sparkle_talents.nif", "ParticleSpawnPos",1,5)
-	local GeheimW = GetSkillValue("",10)
-	local Feils = GetSkillValue("",9)
-    local Ertrag = math.floor((GeheimW * Feils) / 2)
+	if RemoveItems("", "Optigold", 1) > 0 then
+		GetPosition("Owner", "ParticleSpawnPos")
+		StartSingleShotParticle("particles/sparkle_talents.nif", "ParticleSpawnPos", 1, 5)
+		local GeheimW = GetSkillValue("", 10)
+		local Feils = GetSkillValue("", 9)
+		local Ertrag = math.floor((GeheimW * Feils) / 2)
 
-	if not GetInsideBuilding("", "Lager") then
-	    GetHomeBuilding("","Lager")
+		if not GetInsideBuilding("", "Lager") then
+			GetHomeBuilding("","Lager")
+		end
+		AddItems("Lager", "Gold", Ertrag)
+		local item = "_ITEM_Gold_NAME_+0"
+		MsgNewsNoWait("","Lager","","economie",-1,
+					"@L_HPFZ_ARTEFAKT_OPTSCH_NUTZER_KOPF_+0",
+					"@L_HPFZ_ARTEFAKT_OPTSCH_NUTZER_RUMPF_+0", GetID("Lager"), Ertrag, item)
 	end
-    AddItems("Lager","Gold",Ertrag)
-    local item = "_ITEM_Gold_NAME_+0"
-	MsgNewsNoWait("","Lager","","economie",-1,
-				"@L_HPFZ_ARTEFAKT_OPTSCH_NUTZER_KOPF_+0",
-				"@L_HPFZ_ARTEFAKT_OPTSCH_NUTZER_RUMPF_+0",GetID("Lager"),Ertrag,item)
-				
+		
 	StopMeasure()
 	
 end
 
 function CleanUp()
 	StopAnimation("")
-	CarryObject("","",false)
+	CarryObject("", "", false)
 end
 
 function GetOSHData(MeasureID)
-	OSHSetMeasureRepeat("@L_ONSCREENHELP_7_MEASURES_TIMEINFOS_+2",Gametime2Total(mdata_GetTimeOut(MeasureID)))
+	OSHSetMeasureRepeat("@L_ONSCREENHELP_7_MEASURES_TIMEINFOS_+2", Gametime2Total(mdata_GetTimeOut(MeasureID)))
 end
