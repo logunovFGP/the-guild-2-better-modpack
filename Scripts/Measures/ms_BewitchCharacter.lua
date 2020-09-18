d9743db17de71e7abf951441030dbd6116644767
Jean-Luc <thefajeth@gmail.com>
2020-09-18 09:43:30 +0200

    Bewitching fixed (MMP Import)

--- Scripts/Measures/ms_BewitchCharacter.lua
+++ Scripts/Measures/ms_BewitchCharacter.lua
@@ -12,36 +12,65 @@
 function Run()
 
 	if IsStateDriven() then
-		if not GetSettlement("","city") then
+		if not GetSettlement("", "city") then
 			StopMeasure()
 		end
+		
 		if not CityGetNearestBuilding("city", "", -1, GL_BUILDING_TYPE_TAVERN, -1, -1, FILTER_IGNORE, "DestTavern") then
 			StopMeasure()
 		end
+		
+		if GetState("DestTavern", STATE_BUILDING) then
+			StopMeasure()
+		end
+		
+		if not BuildingHasUpgrade("DestTavern", "Divanbed") then
+			StopMeasure()
+		end
+		
+		if not AliasExists("Destination") then
+			StopMeasure()
+		end
+		
+		--LogMessage("BewitchCharacter: "..GetID("").." "..GetName("").." wants to flirt with "..GetID("Destination").." "..GetName("Destination").." ")
 
-		if not f_MoveToNoWait("", "DestTavern", GL_MOVESPEED_RUN) then
+		if not f_MoveTo("", "DestTavern", GL_MOVESPEED_RUN) then
 			StopMeasure()
 		end
-		if not f_MoveToNoWait("Destination", "DestTavern", GL_MOVESPEED_RUN) then
-			StopMeasure()
+		
+		local DesID = GetID("Destination")
+		
+		if GetDistance("Destination","DestTavern") < 1000 then
+			--LogMessage("BewitchCharacter Destination ID: "..DesID.." "..GetName("Destination").." is in range and moves to Tavern")
+			if not f_MoveTo("Destination", "DestTavern", GL_MOVESPEED_RUN) then
+				--LogMessage("BewitchCharacter Destination ID: "..DesID.." error move")				
+				return
+			end
+			f_MoveTo("Destination", "")
+		else
+			--LogMessage("BewitchCharacter: Destination ID: "..DesID.." "..GetName("Destination").." is ported")
+			GetLocatorByName("DestTavern", "Walledge1", "entry")
+			SimBeamMeUp("Destination", "entry", false)
+			f_MoveTo("Destination", "DestTavern", GL_MOVESPEED_RUN)
+			f_MoveTo("Destination", "")
 		end
+			
 
 		local check = true
-		local WaitTime = GetGametime() + 3
+		local WaitTime = math.mod(GetGametime(),24) + 3
 		while check do
 			Sleep(2)
 
-			if not AliasExists("Destination") then
+			if math.mod(GetGametime(),24) > WaitTime then
+				--LogMessage("Bewitching I waited too long, bye")
 				StopMeasure()
-			end
-
-			if WaitTime < GetGametime() then
-				StopMeasure()
+				break
 			end
 
 			if GetInsideBuilding("", "Building1") and GetInsideBuilding("Destination", "Building2") then
-				if (GetID("Building1")==GetID("Building2")) and (GetID("Building1")==GetID("DestTavern")) then
-					if LocatorStatus("DestTavern","Divanbed1",true)==1 then
+				if (GetID("Building1") == GetID("Building2")) and (GetID("Building1") == GetID("DestTavern")) then
+					if LocatorStatus("DestTavern", "Divanbed1", true)==1 then
+						--LogMessage("All set up, lets go bewitching my love")
 						check = false
 					end
 				else
@@ -59,20 +88,20 @@ function Run()
 	local MeasureID = GetCurrentMeasureID("")
 	local TimeUntilRepeat = mdata_GetTimeOut(MeasureID)
 	
-	-- calculate the price according to the basicprice and the rank of the sims
-	-- local Price1 = SimGetRank("Destination")
-	-- local Price2 = SimGetRank("Owner")
-	-- local BasicPrice = 20
-	-- local RankPrice = 20
-	-- local OverallPrice = BasicPrice + (Price1 * RankPrice) + (Price2 * RankPrice)
-	local OverallPrice = 200
+	local OverallPrice = 300
 	SetData("Price", OverallPrice)
 	
 	-- The minimum favor of the destination sim to success
-	local CharismaSkill = GetSkillValue("", CHARISMA)
-	local MinimumFavor = 50 - CharismaSkill
-	local FavorWon = 5 + (CharismaSkill * 0.5)
-	local FavorLoss = Rand(4)-9
+	local TitleDifference = (GetNobilityTitle("Destination") - GetNobilityTitle(""))*2
+	local CharismaSkill = (GetSkillValue("", CHARISMA)*2)
+	local MinimumFavor = 60 + TitleDifference - CharismaSkill
+	local FavorWon = 10 + (CharismaSkill/2) + Rand(7)
+	local FavorLoss = -10 - TitleDifference
+	if FavorLoss > -5 then
+		FavorLoss = -5
+	end
+	local FlirtBonus = GetImpactValue("", 52)		-- 52 = FlirtProfi
+	FavorWon = FavorWon + FavorWon * FlirtBonus * 0.01
 	
 	-- the action number for the courting
 	local CourtingActionNumber = 5
@@ -87,31 +116,43 @@ function Run()
 		StopMeasure()
 		return
 	end
+	
 	if not BuildingHasUpgrade("Tavern", "Divanbed") then
 		StopMeasure()
 	end
-
 	
-	GetOutdoorMovePosition("","Tavern","MovePos")
+	--LogMessage("Start bewitching")
+	
+	GetOutdoorMovePosition("", "Tavern", "MovePos")
 	
 	-- Pay if the tavern does not belong to the owners dynasty
 	if GetDynastyID("Tavern") ~= GetDynastyID("") then
-		if not SpendMoney("", GetData("Price"), "CostSocial") then
-			MsgQuick("", "@L_GENERAL_MEASURES_BEWITCHCHARACTER_FAILURES_+0", GetID(""), GetData("Price"))
-			StopMeasure()
-			return
+		if DynastyIsPlayer("") then
+			if not SpendMoney("", GetData("Price"), "CostSocial") then
+				MsgQuick("", "@L_GENERAL_MEASURES_BEWITCHCHARACTER_FAILURES_+0", GetID(""), GetData("Price"))
+				StopMeasure()
+				return
+			end
 		end
 		CreditMoney("Tavern",GetData("Price"),"Offering")
+		-- for the balance
+	--	local OldBalance = 0
+	--	if HasProperty("Tavern", "BalanceBewitchingFee") then
+	--		OldBalance = GetProperty("Tavern", "BalanceBewitchingFee")
+	--	end
+	--	SetProperty("Tavern", "BalanceBewitchingFee", (OldBalance+OverallPrice))
 	end	
 	local AnimType = "bench_talk"
 		
 	if not GetLocatorByName("Tavern", "Divanbed1", "Bewitcher") then
 		MsgQuick("", "@L_GENERAL_MEASURES_BEWITCHCHARACTER_FAILURES_+1", GetID("Tavern"))
+		--LogMessage("Bewitching Error no Locator 1")
 		StopMeasure()
 		return
 	end
 
 	if not GetLocatorByName("Tavern", "Divanbed2", "Bewitched") then
+		--LogMessage("Bewitching Error no Locator 2")
 		MsgQuick("", "@L_GENERAL_MEASURES_BEWITCHCHARACTER_FAILURES_+1", GetID("Tavern"))
 		StopMeasure()
 		return
@@ -144,7 +185,7 @@ function Run()
 	------ Court Lover ------
 	-------------------------
 	if SimGetCourtLover("", "CourtLover") then
-		if GetID("CourtLover")==GetID("Destination") then
+		if GetID("CourtLover") == GetID("Destination") then
 					
 			WasCourtLover = 1
 			local ModifyFavor = FavorWon
@@ -175,15 +216,15 @@ function Run()
 						if GetID("CurrentBuilding") ~= GetID("Tavern") then
 							f_ExitCurrentBuilding("")
 							f_ExitCurrentBuilding("Destination")
-							f_FollowNoWait("Destination","",150)
-							f_MoveTo("","MovePos")
+							f_FollowNoWait("Destination", "", 150)
+							f_MoveTo("", "MovePos")
 						end
 					else
-						f_FollowNoWait("Destination","",150)
-						f_MoveTo("","MovePos")
+						f_FollowNoWait("Destination", "", 150)
+						f_MoveTo("", "MovePos")
 					end
 					--f_MoveToNoWait("", "Bewitcher")
-					if not SendCommandNoWait("Destination","MoveToPosition") then
+					if not SendCommandNoWait("Destination", "MoveToPosition") then
 						StopMeasure()
 					end
 					SetData("BewitchedLocatorInUse", 1)
@@ -194,10 +235,10 @@ function Run()
 					while not HasData("BewitchedLocatorInUse") do
 						Sleep(1)
 					end
-					CreateCutscene("default","cutscene")
-					CutsceneAddSim("cutscene","")
-					CutsceneAddSim("cutscene","destination")
-					CutsceneCameraCreate("cutscene","")				
+					CreateCutscene("default", "cutscene")
+					CutsceneAddSim("cutscene", "")
+					CutsceneAddSim("cutscene", "Destination")
+					CutsceneCameraCreate("cutscene", "")				
 					camera_CutscenePlayerLockSit("cutscene", "")
 					FadeInFE("", "eyelids_up", 0.5, 0.3, 1)
 					FadeInFE("", "smile", 0.7, 0.4, 0)
@@ -211,7 +252,7 @@ function Run()
 					Sleep(TimeAnim-1.5)
 				end
 				
-				if GetID("cutscene")>0 then
+				if GetID("cutscene") > 0 then
 					camera_CutscenePlayerLockSit("cutscene", "Destination")
 				end
 				feedback_OverheadCourtProgress("Destination", CourtingProgress)								
@@ -223,6 +264,10 @@ function Run()
 			-- Add the archieved progress
 			SetMeasureRepeat(TimeUntilRepeat)
 			chr_ModifyFavor("Destination", "", ModifyFavor)
+		--	if ModifyFavor > 5 then
+		--		AddImpact("", "LoveLevel", 10, 24) -- add some love for the next 24 hours
+		--		AddImpact("Destination", "LoveLevel", 10, 24)
+		--	end
 			SimAddCourtingProgress("")
 			
 		end
@@ -237,24 +282,36 @@ function Run()
 		local success = (GetFavorToSim("Destination", "Owner") > MinimumFavor)
 		if success then
 			
+		--	if SimGetSpouse("Destination", "Spouse") then
+		--		if (GetID("Spouse") == GetID("")) then
+		--			AddImpact("", "LoveLevel", 10, 24) -- add some love for the next 24 hours
+		--			AddImpact("Destination","LoveLevel",10,24)
+		--			if GetImpactValue("Destination", "LoveLevel") >= 10 then
+		--				MsgNewsNoWait("", "Destination", "", "schedule", -1,
+		--						"@L_FAMILY_2_COHIBITATION_FULLOFLOVE_HEAD_+0",
+		--						"@L_FAMILY_2_COHIBITATION_FULLOFLOVE_BODY_+0", GetID("Destination"))
+		--			end
+		--		end
+		--	end
+			
 			-- Go to the divanbed
-			if GetInsideBuilding("","CurrentBuilding") then
+			if GetInsideBuilding("", "CurrentBuilding") then
 				if GetID("CurrentBuilding") ~= GetID("Tavern") then
 					f_ExitCurrentBuilding("")
 					f_ExitCurrentBuilding("Destination")
-					f_FollowNoWait("Destination","",200)
-					f_MoveTo("","MovePos")
+					f_FollowNoWait("Destination", "", 200)
+					f_MoveTo("", "MovePos")
 				end
 			else
-				f_FollowNoWait("Destination","",200)
-				f_MoveTo("","MovePos")
+				f_FollowNoWait("Destination", "", 200)
+				f_MoveTo("", "MovePos")
 			end
 			
 			--f_MoveToNoWait("", "Bewitcher")
 --			if not f_BeginUseLocator("Destination", "Bewitched", GL_STANCE_SIT, true) then
 --				StopMeasure()
 --			end
-			if not SendCommandNoWait("Destination","MoveToPosition") then
+			if not SendCommandNoWait("Destination", "MoveToPosition") then
 				StopMeasure()
 			end
 			if not f_BeginUseLocator("", "Bewitcher", GL_STANCE_SITBENCH, true) then
@@ -266,10 +323,10 @@ function Run()
 				Sleep(1)
 			end
 			
-			CreateCutscene("default","cutscene")
-			CutsceneAddSim("cutscene","")
-			CutsceneAddSim("cutscene","destination")
-			CutsceneCameraCreate("cutscene","")
+			CreateCutscene("default", "cutscene")
+			CutsceneAddSim("cutscene", "")
+			CutsceneAddSim("cutscene", "destination")
+			CutsceneCameraCreate("cutscene", "")
 			camera_CutscenePlayerLockSit("cutscene", "")
 			local TimeAnim = 0					
 			FadeInFE("", "eyelids_up", 0.5, 0.3, 1)
@@ -300,6 +357,7 @@ function Run()
 			
 		end
 	end
+	chr_GainXP("",GetData("BaseXP"))
 	StopMeasure()
 	
 	
@@ -345,6 +403,6 @@ end
 function GetOSHData(MeasureID)
 	--can be used again in:
 	OSHSetMeasureRepeat("@L_ONSCREENHELP_7_MEASURES_TIMEINFOS_+2",Gametime2Total(mdata_GetTimeOut(MeasureID)))
-	OSHSetMeasureCost("@L_INTERFACE_HEADER_+6",200)
+	OSHSetMeasureCost("@L_INTERFACE_HEADER_+6",300)
 end
 
