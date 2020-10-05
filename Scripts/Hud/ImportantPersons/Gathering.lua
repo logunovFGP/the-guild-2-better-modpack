c298733fdf039441bd3c90dc8204547da5e51604
Jean-Luc <thefajeth@gmail.com>
2020-10-05 12:51:38 +0200

    Important persons list

--- Scripts/Hud/ImportantPersons/Gathering.lua
+++ Scripts/Hud/ImportantPersons/Gathering.lua
@@ -2,19 +2,36 @@
 
 function ImportantPersonsAddDynMemberToSection(DynastyReference, Section)
 
-	local iQuantity = DynastyGetMemberCount(DynastyReference)
-	for iCount=0, iQuantity-1 do
-		if DynastyGetMember(DynastyReference, iCount, "dynAlias") then
-			SetImportantPersonToSection(GetID("dynAlias"), Section, GetDynastyID(""))
+	if not DynastyIsDead(DynastyReference) then 
+		local iQuantity = DynastyGetMemberCount(DynastyReference)
+		local Added = false -- do we have already added 1 member?
+		if iQuantity > 0 then
+			for iCount=0, iQuantity-1 do
+				if DynastyGetFamilyMember(DynastyReference, iCount, "dynMember") then
+					if SimGetAge("dynMember") >= 16 and not GetState("dynMember", STATE_DEAD) then
+						if not Added then
+							SetImportantPersonToSection(GetID("dynMember"), Section, GetDynastyID(""))
+							Added = true
+						else
+							if DynastyIsShadow("dynMember") then
+								if SimGetOfficeLevel("dynMember")>=0 then
+									SetImportantPersonToSection(GetID("dynMember"), Section, GetDynastyID(""))
+								end
+							else
+								SetImportantPersonToSection(GetID("dynMember"), Section, GetDynastyID(""))
+							end
+						end
+					end
+				end
+			end
 		end
 	end
 end
 
-
 function ImportantPersonsDiplomacyFilter(DiplState, Section)
 	
 	GetDynasty("", "dynasty")
-	local iCount = ScenarioGetObjects("Dynasty", 99, "Dynasties")
+	local iCount = ScenarioGetObjects("Dynasty", 500, "Dynasties")
 	
 	if iCount==0 then
 		return
@@ -23,35 +40,279 @@ function ImportantPersonsDiplomacyFilter(DiplState, Section)
 	for dyn=0, iCount-1 do
 		Alias = "Dynasties"..dyn
 		if not (GetID(Alias)==GetID("dynasty")) then
-			if DynastyGetDiplomacyState("dynasty", Alias)==DiplState then
-				gathering_ImportantPersonsAddDynMemberToSection(Alias, Section)
+			-- am I an important dynasty?
+			if DynastyGetBuildingCount(Alias,-1,-1)>0 then
+				if DynastyGetDiplomacyState("dynasty", Alias)==DiplState then
+					gathering_ImportantPersonsAddDynMemberToSection(Alias, Section)
+				end
 			end
 		end
 	end
 end
 
-
 function ImportantPersonsSetupSections()
+
+	-- Bookmarked
+	CreateImportantPersonSection("Bookmarks", "@L_IMPORTANTPERSONS_BOOKMARKS_+0")
+	
+	-- people who lended money in your bankhouse and kept it
+	CreateImportantPersonSection("CreditorSearch", "@L_Creditors")
+		
 	-- family
 	CreateImportantPersonSection("Family", "@L_IMPORTANTPERSONS_TOPICS_+0")
 	CreateImportantPersonSection("CourtLover", "@L_IMPORTANTPERSONS_TOPICS_+1")
 	CreateImportantPersonSection("Liaison", "@L_IMPORTANTPERSONS_TOPICS_+2")
+	
+	 -- Fajeth_Mod: unemployed List by Nommy
+	CreateImportantPersonSection("UnemployedByAge", "@L_IMPORTANTPERSONS_AGE_+0")
+	--CreateImportantPersonSection("UnemployedByLevel", "@L_IMPORTANTPERSONS_LEVEL_+0")
+	-- Mod end
 	
 	-- cutscenes
 	CreateImportantPersonSection("DuellGegner", "@L_IMPORTANTPERSONS_TOPICS_+5")
 	CreateImportantPersonSection("ProcessMember", "@L_IMPORTANTPERSONS_TOPICS_+6")
 	CreateImportantPersonSection("OfficeSession", "@L_IMPORTANTPERSONS_TOPICS_+7")
 	
-	-- gunst
+	-- favor
 	CreateImportantPersonSection("TopCandidates", "@L_IMPORTANTPERSONS_TOPICS_+11")
 	CreateImportantPersonSection("Alliance", "@L_IMPORTANTPERSONS_TOPICS_+3")
 	CreateImportantPersonSection("Nap", "@L_IMPORTANTPERSONS_TOPICS_+9")
 	CreateImportantPersonSection("Neutral", "@L_IMPORTANTPERSONS_TOPICS_+10")
 	CreateImportantPersonSection("Enemies", "@L_IMPORTANTPERSONS_TOPICS_+4")
 	
-
 end
 
+-- CREDITORS who didn't pay back by Serp and Fajeth
+function IsCreditorSim(Alias)
+
+	local IsCreditor = false  
+	local bankID
+	if HasProperty(Alias,"StolenSum") then -- if the sim is creditor in any bank
+		bankID = GetProperty(Alias,"CreditBank")  -- the bankID from the creditors bank
+		GetDynasty("","MyDyn")
+		for i=0, DynastyGetBuildingCount2("MyDyn")-1 do  -- loop through all your buildings
+			DynastyGetBuilding2("MyDyn",i,"Bank") -- save the building in "Bank"
+			if BuildingGetType("Bank")==43 then -- check if it is really a bankhouse
+				if GetID("Bank") == bankID then  -- check if your bank is the creditors bank
+					IsCreditor = true
+				end
+			end
+		end
+	end
+	return IsCreditor
+end
+
+function IsBookmarked(Alias)
+	local dyn = GetDynastyID("")
+	local prop = "bm"..dyn
+	
+	if HasProperty(Alias, prop) then
+		return true
+	end
+	
+	return false
+end
+
+-- Called by game to populate the "Creditors" section... at the moment sorted by level and age 
+function ImportantPersonsGather_CreditorSearch()
+	-- Set Debug = true to show messages in game e.g number of people which matched etc.
+	local Debug = false
+	if Debug then MsgQuick("", "ImportantPersonsGather_CreditorSearch()") end
+	
+	-- Define the criteria sims must meet to be included.
+	local SimListFilterFunction = gathering_IsCreditorSim
+	
+	-- Define the sims list sort order.
+	local SimListSortCompareFunction = 
+	function(a,b) 
+		return SimGetAge(a) <= SimGetAge(b) 
+	end
+	
+	-- Find, sort and add the sims.
+	gathering_PopulateImportantPersonSection("CreditorSearch", SimListFilterFunction, SimListSortCompareFunction, Debug)
+end
+
+-- Called by game to populate the "Creditors" section... at the moment sorted by level and age 
+function ImportantPersonsGather_Bookmarks()
+	-- Set Debug = true to show messages in game e.g number of people which matched etc.
+	local Debug = false
+	if Debug then MsgQuick("", "ImportantPersonsGather_Bookmarks()") end
+	  
+		-- Define the criteria sims must meet to be included.
+		local SimListFilterFunction = gathering_IsBookmarked
+	  
+		-- Define the sims list sort order.
+		local SimListSortCompareFunction = 
+		function(a,b) 
+		return SimGetAge(a) <= SimGetAge(b) 
+	end
+	  
+	  -- Find, sort and add the sims.
+	  gathering_PopulateImportantPersonSection("Bookmarks", SimListFilterFunction, SimListSortCompareFunction, Debug)
+end
+
+-- By Nommy: http://forum.runeforge-games.net/index.php/topic,840.0.html
+
+-- Unemployed List by Nommy
+-- Returns true if Alias is an unemployed sim living in the same city as you. Determines what sims are incuded in the unemployed sections.
+-- Note: Presumably it's the same settlement as the one in which the first sim in your groups lives, but IDK exactly what GetSettlementID("") does.
+function IsUnemployedSim(Alias)
+	
+	local IsUnemployed = false
+	if not IsDynastySim(Alias)                              -- Is not a member of a dynasty
+		and not DynastyIsShadow(Alias) -- no shadows
+		and GetDynastyID(Alias) < 1
+		and (SimGetProfession(Alias) == 0)                      -- Is unemployed
+		and (SimGetAge(Alias) >= 20)                            -- Is old enough to work
+		and (GetSettlementID(Alias) == GetSettlementID(""))     -- Lives in same settlement
+		and not SimGetCourtLover(Alias, "MyLover")
+		and not SimGetSpouse(Alias, "MyLoverr")
+		and not GetStateImpact(Alias, "no_hire")
+		and not HasProperty(Alias, "courted")
+		and not HasProperty(Alias, "NotAffectable")
+		and not GetState(Alias, STATE_NPC)
+		and not GetState(Alias, STATE_NPCFIGHTER)
+		and not GetState(Alias, STATE_TOWNNPC)
+		
+	then
+		IsUnemployed = true
+		SimSetHireable(Alias, true)
+	end
+	
+	return IsUnemployed
+end
+
+
+-- Called by game to populate the "UnemployedByAge" section.
+function ImportantPersonsGather_UnemployedByAge()
+	local Debug = false
+	if Debug then MsgQuick("", "ImportantPersonsGather_UnemployedByAge()") end
+	
+	-- Define the criteria sims must meet to be included.
+	local SimListFilterFunction = gathering_IsUnemployedSim
+	
+	-- Define the sims list sort order.
+	local SimListSortCompareFunction = 
+		function(a,b) 
+			return SimGetAge(a) <= SimGetAge(b)
+		end
+	
+	-- Find, sort and add the sims.
+	gathering_PopulateImportantPersonSection("UnemployedByAge", SimListFilterFunction, SimListSortCompareFunction, Debug)
+end
+
+
+-- Called by game to populate the "UnemployedByLevel" section.
+function ImportantPersonsGather_UnemployedByLevel()
+	local Debug = false
+	if Debug then MsgQuick("", "ImportantPersonsGather_UnemployedByLevel()") end
+	
+	-- Define the criteria sims must meet to be included.
+	local SimListFilterFunction = gathering_IsUnemployedSim
+	
+	-- Define the sims list sort order.
+	local SimListSortCompareFunction = 
+		function(a,b) 
+			return SimGetLevel(a) > SimGetLevel(b)
+		end
+	
+	-- Find, sort and add the sims.
+	gathering_PopulateImportantPersonSection("UnemployedByLevel", SimListFilterFunction, SimListSortCompareFunction, Debug)
+end
+
+
+-- gathering_PopulateImportantPersonSection()
+-- Finds, sorts and adds sims to the specified section using the provided filter and sort critera.
+-- Arguments:
+--     Label                        Section label. Should be same as first argument passed to CreateImportantPersonSection() in ImportantPersonsSetupSections().
+--     SimListFilterFunction        Function defining the critera sims are required to match to be included in the list.
+--                                     The function should take 1 argument (the sim alias string) and return true when the sim is to be included or false otherwise.
+--                                     e.g:    function(Alias) return SimGetProfession(Alias) == 10 end
+--     SimListSortCompareFunction   (optional) comparison function used to sort the elements. See compare argument of QuickSort() function below for more details.
+--     Debug                        (optional) Whether to display debug messages in the game (sim counts etc). Default = false (no).
+--
+function PopulateImportantPersonSection(Label, SimListFilterFunction, SimListSortCompareFunction, Debug)	
+	-- Assign missing arguments default values
+	SimListSortCompareFunction = SimListSortCompareFunction or function(a,b) return SimGetAge(a)<=SimGetAge(b) end
+	Debug = Debug or false
+	-- Assign control values
+	local MaxSearchResults = -1  -- Number of sims returned by search is unlimited.
+	
+	-- Get a list of all sims in the world. (I couldn't get Find() to work, so resorted to this.)
+	local GlobalSimCount = ScenarioGetObjects("cl_Sim", MaxSearchResults, "Sims")
+	if Debug then MsgQuick("", "PopulateImportantPersonSection(\""..Label.."\", ...)   GlobalSimCount="..GlobalSimCount) end
+	if GlobalSimCount == 0 then
+		return
+	end
+	
+	-- Filter the list to only include sims which match the supplied filter function.
+	local FilteredSims = {}
+	local FilteredSimCount = 0
+	local Alias
+	for i = 0, GlobalSimCount-1 do
+		Alias = "Sims"..i
+		if SimListFilterFunction(Alias) then
+			FilteredSimCount = FilteredSimCount + 1
+			FilteredSims[FilteredSimCount] = Alias
+			-- show 8 max by Fajeth
+			if FilteredSimCount>=8 then
+				break
+			end
+		end
+	end
+	if Debug then MsgQuick("", "FilteredSimCount="..FilteredSimCount) end
+	
+	-- Sort the list of sims (by age by default).
+	-- Note: table.sort() and other table functions don't seem to work so the quicksort function defined below is used instead.
+	gathering_QuickSort(FilteredSims, 1, FilteredSimCount, SimListSortCompareFunction)
+	
+	-- Add the sorted list of sims to the specified section.
+	local Classes = ""
+	for i = 1, FilteredSimCount do
+		Alias = FilteredSims[i]
+		SetImportantPersonToSection(GetID(Alias), Label, GetDynastyID(""))
+		Classes = Classes.." "..SimGetClass(Alias)
+	end
+	if Debug then MsgQuick("", "Classes="..Classes) end
+end
+
+
+
+-- gathering_QuickSort()
+-- Sorts a table opionally using specified comparison function.
+-- http://rosettacode.org/wiki/Sorting_algorithms/Quicksort#Lua
+-- Arguments:
+--    t        table to be sorted
+--    start    first element index
+--    endi     last element index
+--    compare  (optional) comparison function used to sort the elements. The function should take 2 arguments and
+--             should return true when the first is less than or equal to the second and false otherwise. The default is:
+--             function(a,b) return a<=b end
+-- 
+function QuickSort(t, start, endi, compare)
+  start = start or 1
+  compare = compare or function(a,b) return a<=b end
+  -- partition w.r.t. first element
+  if(endi - start < 2) then return t end
+  local pivot = start
+  for i = start + 1, endi do
+    -- equivalent of:   if t[i] <= t[pivot] then
+    if compare(t[i], t[pivot]) then
+      local temp = t[pivot + 1]
+      t[pivot + 1] = t[pivot]
+      if(i == pivot + 1) then
+        t[pivot] = temp
+      else
+        t[pivot] = t[i]
+        t[i] = temp
+      end
+      pivot = pivot + 1
+    end
+  end
+  t = gathering_QuickSort(t, start, pivot - 1, compare)
+  return gathering_QuickSort(t, pivot + 1, endi, compare)
+end
+-- Mod end
 
 
 function ImportantPersonsGather_Family()
