a61c245515b3f4c3cd9c9f68aa62ebe6f97b3afa
Jean-Luc <thefajeth@gmail.com>
2020-10-07 14:05:29 +0200

    Fajeth hotfix

--- Scripts/Measures/Behaviour/std_Idle.lua
+++ Scripts/Measures/Behaviour/std_Idle.lua
@@ -21,7 +21,7 @@ function Run()
 	
 	if not GetHomeBuilding("", "HomeBuilding") then
 		if not GetSettlement("", "MyCity") then
-			Get NearestSettlement("", "MyCity")
+			GetNearestSettlement("", "MyCity")
 		end
 		CityGetNearestBuilding("MyCity", "", -1, GL_BUILDING_TYPE_WORKER_HOUSING, -1, -1, FILTER_IGNORE, "NewHome")
 		
