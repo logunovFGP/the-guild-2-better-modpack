12453522eb9c3f2ee314a9c1b73cce2338705e96
Fajeth <thefajeth@gmail.com>
2020-11-24 20:56:42 +0100

    Thugs remove pamphlets minimod (MMP merge)

--- Scripts/Measures/ms_147_PatrolTheTown.lua
+++ Scripts/Measures/ms_147_PatrolTheTown.lua
@@ -10,25 +10,25 @@
 function Init() 
 	local i = 2
 	local Distance
-	CopyAlias("Destination","Waypoint1")
+	CopyAlias("Destination", "Waypoint1")
 	--collect the waypoints
 	if AliasExists("Waypoint1") then
 		while true do 
 			InitAlias("Waypoint"..i,MEASUREINIT_SELECTION, "__F((Object.Type == Building) OR (Object.Type == Position))",
 				"@L_GENERAL_MEASURES_147_PATROLTHETOWN_WAYPOINT_+0", AIInit)
-			--GetPosition("Waypoint"..i,"ParticleSpawnPos")
-			--StartSingleShotParticle("particles/aimingpoint.nif", "ParticleSpawnPos",4,5)
-			Distance = GetDistance("Waypoint"..i,"Waypoint"..i-1)
+			GetPosition("Waypoint"..i, "ParticleSpawnPos")
+			StartSingleShotParticle("particles/aimingpoint.nif", "ParticleSpawnPos", 4, 5)
+			Distance = GetDistance("Waypoint"..i, "Waypoint"..i-1)
 			i = i + 1
-			if Distance < 50 then
+			if Distance < 200 then
 				break
 			end	
 			Sleep(1)
 		end
 	end
 	i = i-1
-	SetData("WaypointCount",i)
-	MsgMeasure("","")
+	SetData("WaypointCount", i)
+	MsgMeasure("", "")
 end
 
 function AIInit()
@@ -36,20 +36,18 @@ end
 
 function Run()
 	local Count
-	local i
+
 	if AliasExists("Waypoint1") then
 		Count = 0+GetData("WaypointCount")
 	end
+
 	--ai timeout
-	local	TimeOut
-	TimeOut = GetData("TimeOut")
+	local TimeOut = GetData("TimeOut")
 	if TimeOut then
 		TimeOut = GetGametime() + TimeOut
 	end
 	
-	
 	while true do
-	
 		if TimeOut then
 			if TimeOut < GetGametime() then
 				break
@@ -59,11 +57,12 @@ function Run()
 		--for the ai
 		if not AliasExists("Waypoint1") then
 			if (GetOutdoorMovePosition("", "Destination", "Target")) then
-				if not (f_MoveTo("", "Target", GL_MOVESPEED_WALK, 200)) then
+				if not (f_MoveTo("", "Target", GL_MOVESPEED_WALK, 100)) then
 					return
 				end
 			end
-			f_Stroll("", 500, 20)
+			f_Stroll("", 600, 20)
+			Sleep(45)
 			return
 		else
 			--if only one waypoint, and waypoint1 is building
@@ -73,23 +72,109 @@ function Run()
 					if GetLocatorByName("Waypoint1", "Walledge"..k, "VictimsCorner"..k) then
 						f_MoveTo("", "VictimsCorner"..k, GL_MOVESPEED_WALK, 10)
 					end
-					Sleep(Rand(3)+1)
+					Sleep(Rand(5)+10)
 				end
 			else
 				
 				--walk through all waypoints
-				i = 1
-				for i=1,Count do
-					f_MoveTo("","Waypoint"..i)
-					Sleep(1)
+				for i=1, Count do
+					f_MoveTo("", "Waypoint"..i)
+					Sleep(10)
 				end
 			end
-	
+		end
+		
+		--check for blackboard
+		if SimGetWorkingPlace("", "Workbuilding") then
+			if BuildingGetOwner("Workbuilding", "Boss") then
+				if DynastyIsPlayer("Boss") then
+					local BlackBoard = false
+					if FindNearestBuilding("", 3, 41, 0, false, "BlackBoardNear") then
+						local cost = GetNobilityTitle("Boss")*50
+						if (GetDistance("", "BlackBoardNear") <= 800) and GetMoney("Boss") >= cost then
+							BlackBoard = true
+							--check for pamphlets and remove them
+							ms_147_patrolthetown_RemovePamphlet("BlackBoardNear", cost)
+						end
+					end
+				end	
+			end
 		end
 		Sleep(5)
 	end
 end
 
+-- patrolling myrmidon should remove a pamphlet against his dynasty
+--added by Napi & Fajeth
+
+function RemovePamphlet(blackboard, cost)
+	
+	--check pamphlets (from di_RemovePamphlet.lua)
+	for i=0, 3 do
+		if HasProperty(blackboard,"Pamphlet_"..i) then
+			local PamphletID = GetProperty(blackboard, "Pamphlet_"..i)
+			if GetAliasByID(PamphletID, "PamphletVictim") then
+				if (GetDynastyID("PamphletVictim") == GetDynastyID("")) then
+					SetData("PamphletIdx", i)
+				end
+			end
+		end
+	end
+	
+	--
+	
+	local Idx
+	if HasData("PamphletIdx") then
+		Idx = GetData("PamphletIdx")
+	else
+		return
+	end
+
+	if not HasProperty(blackboard, "Pamphlet_"..Idx) then
+		return		
+	end
+	
+	-- animation stuff
+	
+	GetLocatorByName(blackboard, "entry1", "MovePos")
+	f_MoveTo("", "MovePos", GL_MOVESPEED_RUN)
+	AlignTo("", blackboard)
+	Sleep(1)
+	PlayAnimationNoWait("", "manipulate_middle_up_r")
+	MsgSay("", "@L_PATROLTOWN_PAMPHLET_REMOVE_SAY", GetID("Boss"))
+	Sleep(1)
+	
+	if not HasProperty(blackboard, "Pamphlet_"..Idx) then
+		return		
+	end
+	
+	-- remove pamphlet if you have enough money
+	
+	if GetMoney("Boss") >= cost then
+		if BlackBoardRemovePamphlet(blackboard, Idx) then
+			chr_SpendMoney("Boss", cost, "CostBribes", true)
+
+			if HasProperty(blackboard, "Pamphlet_"..Idx) then
+				RemoveProperty(blackboard, "Pamphlet_"..Idx)
+			end
+
+			if HasProperty(blackboard, "Pamphlet_"..Idx.."Dur") then
+				RemoveProperty(blackboard, "Pamphlet_"..Idx.."Dur")
+			end
+
+			IncrementXPQuiet("", 15)
+		
+			--message
+			SimGetWorkingPlace("", "Workbuilding")
+			BuildingGetOwner("Workbuilding", "Boss")
+		
+			MsgNewsNoWait("Boss", "", "", "intrigue" , -1,
+				"@L_PATROLTOWN_PAMPHLET_REMOVE_SUCCESS_HEAD_+0",
+				"@L_PATROLTOWN_PAMPHLET_REMOVE_SUCCESS_BODY_+0",
+				GetID(""), GetID("Boss"), cost)
+		end
+	end
+end
 
 function CleanUp()
 end
