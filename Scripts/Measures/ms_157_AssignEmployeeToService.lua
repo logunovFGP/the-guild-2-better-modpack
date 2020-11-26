8731eb1327026404b42de001287c3eabb5ad4299
Fajeth <thefajeth@gmail.com>
2020-11-26 23:37:51 +0100

    Tavern: Service (CleanUp + AliasCheck)

--- Scripts/Measures/ms_157_AssignEmployeeToService.lua
+++ Scripts/Measures/ms_157_AssignEmployeeToService.lua
@@ -10,21 +10,26 @@
 -- Run
 -- -----------------------
 function Run()
-	if not SimGetWorkingPlace("","Tavern") then
+	if not SimGetWorkingPlace("", "Tavern") then
 		if IsPartyMember("") then
-			if not GetInsideBuilding("","CurrentBuilding") then
-				StopMeasure()
+			if not GetInsideBuilding("", "CurrentBuilding") then
+				return
 			end
-			if BuildingGetType("CurrentBuilding")==GL_BUILDING_TYPE_TAVERN then
-				CopyAlias("CurrentBuilding","Tavern")
+			if BuildingGetType("CurrentBuilding") == GL_BUILDING_TYPE_TAVERN then
+				CopyAlias("CurrentBuilding", "Tavern")
 			else
-				StopMeasure()
+				return
 			end
 		else
-			StopMeasure()
+			return
 		end
 	end
-	SetProperty("Tavern","ServiceActive",1)
+
+	if not AliasExists("Tavern") then
+		return
+	end
+
+	SetProperty("Tavern", "ServiceActive", 1)
 	
 	SetData("IsProductionMeasure", 0)
 	SimSetProduceItemID("", -GetCurrentMeasureID(""), -1)
@@ -44,22 +49,22 @@ end
 function CleanTables()
 	local Type = Rand(4)
 	if Type == 0 then	
-		GetFreeLocatorByName("Tavern","ServeSitRich",0,3,"MovePos")
-		f_BeginUseLocator("","MovePos",GL_STANCE_STAND,true)
-		PlayAnimation("","manipulate_middle_twohand")
-		f_EndUseLocator("","MovePos",GL_STANCE_STAND)
+		GetFreeLocatorByName("Tavern", "ServeSitRich", 0, 3,"MovePos")
+		f_BeginUseLocator("", "MovePos", GL_STANCE_STAND, true)
+		PlayAnimation("", "manipulate_middle_twohand")
+		f_EndUseLocator("", "MovePos", GL_STANCE_STAND)
 	elseif Type == 1 then
-		GetFreeLocatorByName("Tavern","ServeStand",0,5,"MovePos")
-		f_BeginUseLocator("","MovePos",GL_STANCE_STAND,true)
-		PlayAnimation("","manipulate_middle_twohand")
-		f_EndUseLocator("","MovePos",GL_STANCE_STAND)
+		GetFreeLocatorByName("Tavern", "ServeStand", 0, 5, "MovePos")
+		f_BeginUseLocator("", "MovePos", GL_STANCE_STAND, true)
+		PlayAnimation("", "manipulate_middle_twohand")
+		f_EndUseLocator("", "MovePos", GL_STANCE_STAND)
 	elseif Type == 2 then
-		GetFreeLocatorByName("Tavern","ServeAloneStand",-1,-1,"MovePos")
-		f_BeginUseLocator("","MovePos",GL_STANCE_STAND,true)
-		PlayAnimation("","manipulate_middle_twohand")
-		f_EndUseLocator("","MovePos",GL_STANCE_STAND)
+		GetFreeLocatorByName("Tavern", "ServeAloneStand", -1, -1, "MovePos")
+		f_BeginUseLocator("", "MovePos", GL_STANCE_STAND, true)
+		PlayAnimation("", "manipulate_middle_twohand")
+		f_EndUseLocator("", "MovePos", GL_STANCE_STAND)
 	else
-		PlayAnimation("","cogitate")
+		PlayAnimation("", "cogitate")
 	end
 end
 
@@ -68,44 +73,46 @@ function Serve()
 	local Locator
 	if Type == 0 then
 		Locator = Rand(4)
-		GetLocatorByName("Tavern","ServeSitRich"..Locator,"MovePos")
-		f_BeginUseLocator("","MovePos",GL_STANCE_STAND,true)
-		PlayAnimation("","talk_short")
-		f_EndUseLocator("","MovePos",GL_STANCE_STAND)
+		GetLocatorByName("Tavern", "ServeSitRich"..Locator, "MovePos")
+		f_BeginUseLocator("", "MovePos", GL_STANCE_STAND, true)
+		PlayAnimation("", "talk_short")
+		f_EndUseLocator("", "MovePos", GL_STANCE_STAND)
 	else 
 		Locator = Rand(6)
-		GetLocatorByName("Tavern","ServeStand"..Locator,"MovePos")
-		f_BeginUseLocator("","MovePos",GL_STANCE_STAND,true)
-		PlayAnimation("","talk_short")
-		f_EndUseLocator("","MovePos",GL_STANCE_STAND)
+		GetLocatorByName("Tavern", "ServeStand"..Locator, "MovePos")
+		f_BeginUseLocator("", "MovePos", GL_STANCE_STAND, true)
+		PlayAnimation("", "talk_short")
+		f_EndUseLocator("", "MovePos", GL_STANCE_STAND)
 	end
 	Type = Rand(3)
 	if Type == 0 then
-		if GetLocatorByName("Tavern","ServeAloneStand0","ServePos") then
-			f_BeginUseLocator("","ServePos",GL_STANCE_STAND,true)
-			PlayAnimation("","manipulate_middle_twohand")
-			f_EndUseLocator("","ServePos",GL_STANCE_STAND)
+		if GetLocatorByName("Tavern", "ServeAloneStand0", "ServePos") then
+			f_BeginUseLocator("", "ServePos", GL_STANCE_STAND, true)
+			PlayAnimation("", "manipulate_middle_twohand")
+			f_EndUseLocator("", "ServePos", GL_STANCE_STAND)
 		end
 	elseif Type == 1 then
-		if GetLocatorByName("Tavern","ServeAloneHigh0","ServePos") then
-			f_BeginUseLocator("","ServePos",GL_STANCE_STAND,true)
-			PlayAnimation("","manipulate_top_r")
-			f_EndUseLocator("","ServePos",GL_STANCE_STAND)
+		if GetLocatorByName("Tavern", "ServeAloneHigh0", "ServePos") then
+			f_BeginUseLocator("", "ServePos", GL_STANCE_STAND, true)
+			PlayAnimation("", "manipulate_top_r")
+			f_EndUseLocator("", "ServePos", GL_STANCE_STAND)
 		end
 	elseif Type == 2 then
-		if GetLocatorByName("Tavern","ServeAloneKnee0","ServePos") then
-			f_BeginUseLocator("","ServePos",GL_STANCE_STAND,true)
-			PlayAnimation("","manipulate_middle_low_r")
-			f_EndUseLocator("","ServePos",GL_STANCE_STAND)
+		if GetLocatorByName("Tavern", "ServeAloneKnee0", "ServePos") then
+			f_BeginUseLocator("", "ServePos", GL_STANCE_STAND, true)
+			PlayAnimation("", "manipulate_middle_low_r")
+			f_EndUseLocator("", "ServePos", GL_STANCE_STAND)
 		end
 	end
-	f_BeginUseLocator("","MovePos",GL_STANCE_STAND,true)
-	PlayAnimation("","manipulate_middle_low_r")
-	f_EndUseLocator("","MovePos",GL_STANCE_STAND)
+	f_BeginUseLocator("", "MovePos", GL_STANCE_STAND, true)
+	PlayAnimation("", "manipulate_middle_low_r")
+	f_EndUseLocator("", "MovePos", GL_STANCE_STAND)
 	
 end
 
 function CleanUp()
-	RemoveProperty("Tavern","ServiceActive")
+	if AliasExists("Tavern") then
+		RemoveProperty("Tavern", "ServiceActive")
+	end
 end
 
