68d73f574e87d22e5f092920bb3f1fe18086c5bc
Fajeth <thefajeth@gmail.com>
2021-08-23 12:58:06 +0200

    Fixed broken AI management of robbers and thiefes (Vanilla) - also both buildings get a cart - Actual AI is still broken (WIP -> AI rework)

--- Scripts/Buildings/Robber.lua
+++ Scripts/Buildings/Robber.lua
@@ -1,8 +1,4 @@
 function Run()
-	if BuildingGetOwner("", "MyBoss") and BuildingGetCartCount("") < 1 then
-		GetOutdoorMovePosition(nil, "", "Pos")
-		ScenarioCreateCart(EN_CT_MIDDLE, "", "Pos", "NewCart")
-	end
 end
 
 function OnLevelUp()
