a0e5882fefdb531947d1a95b27243efadb5f54d7
Fajeth <thefajeth@gmail.com>
2021-08-30 23:21:10 +0200

    Curse Building fix - wrong Inventory-Type

--- Scripts/States/state_hpfz_verflucht.lua
+++ Scripts/States/state_hpfz_verflucht.lua
@@ -3,41 +3,44 @@ end
 
 function Run()
 
-  local Slots, Number, ItemId, ItemCount, Abzug, Menge
+	local Slots, Number, ItemId, ItemCount, Abzug, Menge
 
-    if GetImpactValue("","verflucht")==1 then
-        GetPosition("","HausFliegen")
-        GfxAttachObject("fliegen", "particles/flies.nif")
-        GfxSetPositionTo("fliegen", "HausFliegen")
-	    GfxMoveToPosition("fliegen",0,20,0,0.1,false)
-	    GfxStartParticle("fliegenfliegen", "particles/flies.nif", "HausFliegen", 2)
+	if GetImpactValue("", "GebaudeVerflucht") == 1 then
+		local dur = ImpactGetMaxTimeleft("", "GebaudeVerflucht")
+		BuildingGetOwner("", "BOwner")
+		SetData("Duration", dur)
+		SetData("Schaden", 0)
+	
+		-- visual
+		GetPosition("", "HausFliegen")
+		GfxAttachObject("fliegen", "particles/flies.nif")
+		GfxSetPositionTo("fliegen", "HausFliegen")
+		GfxMoveToPosition("fliegen", 0, 20, 0, 0.1, false)
+		GfxStartParticle("fliegenfliegen", "particles/flies.nif", "HausFliegen", 2)
 
-        while GetImpactValue("",388)>0 do
-              Slots = InventoryGetSlotCount("",INVENTORY_STD)
-              Number = Rand(Slots)
-              ItemId, ItemCount = InventoryGetSlotInfo("", Number, InventoryType)
-              Abzug = Rand(3)
+		while GetImpactValue("", "GebaudeVerflucht") >0 do
+			Slots = InventoryGetSlotCount("", INVENTORY_STD)
+			Number = Rand(Slots)
+			ItemId, ItemCount = InventoryGetSlotInfo("", Number, INVENTORY_STD)
 
-              if Abzug == 0 then
-                 Menge = (ItemCount / 100) * 20
-                 RemoveItems("",ItemId,Menge,INVENTORY_STD)
-              elseif Abzug == 1 then
-                 Menge = (ItemCount / 100) * 30
-                 RemoveItems("",ItemId,Menge,INVENTORY_STD)
-              elseif Abzug == 2 then
-                 Menge = (ItemCount / 100) * 50
-                 RemoveItems("",ItemId,Menge,INVENTORY_STD)
-              end
-
-              Sleep(30)
-        end
-        SetState("",STATE_HPFZ_VERFLUCHT,false)
-        GfxDetachObject("fliegen")
-        return
-    end
- 
+			if ItemCount > 0 then
+				Menge = Rand(ItemCount)
+				RemoveItems("", ItemId, Menge, INVENTORY_STD)
+				SetData("Schaden", (GetData("Schaden") + Menge))
+				MsgNewsNoWait("", "", "", "intrigue", -1, "@L_HPFZ_VERFLUCHEN_EFFECTMSG_HEAD_+0",
+							"@L_HPFZ_VERFLUCHEN_EFFECTMSG_BODY", GetID("BOwner"), GetID(""), Menge, ItemId)
+			end
+			Sleep(45)
+		end
+		GfxDetachObject("fliegen")
+	end
+    SetState("", STATE_HPFZ_VERFLUCHT, false)
 end
 
 function CleanUp()
-  SetState("",STATE_HPFZ_VERFLUCHT,false)
+	if GetData("Schaden") > 0 then
+		local duration = GetData("Duration")
+		MsgNewsNoWait("All", "", "", "intrigue", -1, "@L_HPFZ_VERFLUCHEN_ENDMSG_HEAD_+0",
+						"@L_HPFZ_VERFLUCHEN_ENDMSG_BODY", GetID("BOwner"), GetID(""), duration, Menge)
+	end		
 end
