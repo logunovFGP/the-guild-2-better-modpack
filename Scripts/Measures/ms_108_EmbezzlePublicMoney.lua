062074fae677a3fb33427f961fcb91cd125ede79
Fajeth <thefajeth@gmail.com>
2021-09-23 15:05:54 +0200

    Balancing: Embezzle public money works the same every round and max 5000 gold

--- Scripts/Measures/ms_108_EmbezzlePublicMoney.lua
+++ Scripts/Measures/ms_108_EmbezzlePublicMoney.lua
@@ -5,45 +5,32 @@ function Run()
 
 	local MeasureID = GetCurrentMeasureID("")
 	local TimeOut = mdata_GetTimeOut(MeasureID)
-	if not GetSettlement("","city") then
-		StopMeasure()
+
+	if not GetSettlement("", "city") then
+		return
 	end
 
 	local CityTreasure = GetMoney("city")
 	local CityTreasureMin = 1000
 	local money = 0
-	local MaxMoney = 0
-	local round = GetRound()
+	local MaxMoney = (CityTreasure - CityTreasureMin)/10
 	
-	if round<3 then
-		MaxMoney = 2000
-	elseif round<6 then
-		MaxMoney = 4000
-	elseif round<9 then
-		MaxMoney = 5000
-	elseif round<12 then
-		MaxMoney = 6000
-	else
-		MaxMoney = 7000
+	if CityTreasure < CityTreasureMin then
+		MsgQuick("", "@L_MEASURE_EMBEZZLEPUBLICMONEY_FAILURE_+0", GetID("city"))
+		return
 	end
 	
-	if CityTreasure<CityTreasureMin then
-		MsgQuick("", "@L_MEASURE_EMBEZZLEPUBLICMONEY_FAILURE_+0", GetID("city"))
-		StopMeasure()
-	else
-		if (CityTreasure-MaxMoney)<CityTreasureMin then
-			money = CityTreasure-CityTreasureMin
-		else
-			money = MaxMoney
-		end
+	if MaxMoney > 5000 then
+		MaxMoney = 5000
 	end
 	
 	GetInsideBuilding("", "Townhall")
-	if GetLocatorByName("Townhall", "EmbezzleMoney","Result") then
+	if GetLocatorByName("Townhall", "EmbezzleMoney", "Result") then
 		f_MoveTo("", "Result")
 	end
-	PlayAnimation("","manipulate_middle_twohand")
-
+	PlayAnimation("", "manipulate_middle_twohand")
+	
+	SetMeasureRepeat(TimeOut)
 	chr_SpendMoney("city", money, "CostThiefs")
 	CreditMoney("", money, "EmbezzledPublicMoney")
 	
@@ -51,11 +38,10 @@ function Run()
 		"@L_PRIVILEGES_108_EMBEZZLEPUBLICMONEY_SUCCESS_HEAD_+0",
 		"@L_PRIVILEGES_108_EMBEZZLEPUBLICMONEY_SUCCESS_BODY_+0", GetID("Owner"), GetID("city"), money)
 
-	SetMeasureRepeat(TimeOut)
+	chr_GainXP("", GetData("BaseXP"))
 
 	--for the mission
 	mission_ScoreCrime("", money)
-	
 end
 
 function GetOSHData(MeasureID)
