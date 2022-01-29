ec823cd0cea108cd353066e04db6f0758c77c81e
Fajeth <thefajeth@gmail.com>
2022-01-29 08:40:13 +0100

    Mdata: secret knowledge fix (Measures ID rework)

--- Scripts/Library/mdata.lua
+++ Scripts/Library/mdata.lua
@@ -6,37 +6,21 @@ function Init()
 end
 
 function GetDuration(MeasureID)
-	local Duration = GetDatabaseValue("Measures",MeasureID,"duration")
+	local Duration = GetDatabaseValue("Measures", MeasureID, "duration")
+	
 	--for artifact duration
-	if (((MeasureID >= 1630) and (MeasureID <= 1990)) or
-	(MeasureID == 10925) or
-	((MeasureID >= 10928) and (MeasureID <= 10930)) or
-	((MeasureID == 10932)) or
-	((MeasureID >= 10961) and (MeasureID <= 10963)) or
-	((MeasureID >= 11001) and (MeasureID <= 11011)) or
-	((MeasureID >= 11093) and (MeasureID <= 11097)) or
-	((MeasureID >= 11968) and (MeasureID <= 11989)) or
-	((MeasureID >= 12052) and (MeasureID <= 12054)) or
-	((MeasureID >= 12063) and (MeasureID <= 12064))) then
-		Duration = Duration + (chr_ArtifactsDuration("",Duration))
+	if (MeasureID >= 5000) and (MeasureID <= 6000) then
+		Duration = Duration + (chr_ArtifactsDuration("", Duration))
 	end
 	return Duration
 end
 
 function GetTimeOut(MeasureID)
-	local TimeOut = GetDatabaseValue("Measures",MeasureID,"repeat_time")
+	local TimeOut = GetDatabaseValue("Measures", MeasureID, "repeat_time")
+	
 	--for artifact timeout
-	if (((MeasureID >= 1630) and (MeasureID <= 1990)) or
-	(MeasureID == 10925) or
-	((MeasureID >= 10928) and (MeasureID <= 10930)) or
-	((MeasureID == 10932)) or
-	((MeasureID >= 10961) and (MeasureID <= 10963)) or
-	((MeasureID >= 11001) and (MeasureID <= 11011)) or
-	((MeasureID >= 11093) and (MeasureID <= 11097)) or
-	((MeasureID >= 11968) and (MeasureID <= 11989)) or
-	((MeasureID >= 12052) and (MeasureID <= 12054)) or
-	((MeasureID >= 12063) and (MeasureID <= 12064))) then
-		TimeOut = TimeOut - (chr_ArtifactsDuration("",TimeOut))
+	if (MeasureID >= 5000) and (MeasureID <= 6000) then
+		TimeOut = TimeOut - (chr_ArtifactsDuration("", TimeOut))
 	end
 	return TimeOut
 end
