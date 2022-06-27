df1be67e551da4cc7430e0a8c75c5e629db0ba12
Fajeth <thefajeth@gmail.com>
2022-06-27 23:18:11 +0200

    Burning Rebalancing

--- Scripts/States/state_burning.lua
+++ Scripts/States/state_burning.lua
@@ -4,30 +4,50 @@ function Init()
 end
 
 function Run()
-  Evacuate("", false) 
+  	Evacuate("", false) 
 	
-	local FireFactor = 1.25 - GetImpactValue("Owner", "ProtectionFromFire")
-	local PercentDamage = 70* FireFactor
-	if PercentDamage <= 0 then
+	local FireProt = GetImpactValue("Owner", "ProtectionFromFire")
+	if FireProt > 0.95 then
+		FireProt = 0.95
+	end
+	
+	local MaxHP = GetMaxHP("")
+	local RandomFire = 25 + Rand(7)*10
+	local Season = GetSeason()
+	
+	-- season may cause more damage or less
+	if Season == EN_SEASON_SUMMER then
+		RandomFire = RandomFire + 20
+	elseif Season == EN_SEASON_WINTER then
+		RandomFire = RandomFire - 10
+	end
+	
+	-- check for rain for lesser damage
+	local RainValue = Weather_GetValue(0)
+	if RainValue > 0 then
+		if RainValue <= 0.5 then
+			RandomFire = RandomFire - 5
+		else
+			RandomFire = RandomFire - RainValue*10
+		end
+	end
+	
+	-- protection may protect 100% against weak fires
+	if RandomFire < (FireProt*100) then
 		return
 	end
-	local BurnToHP = 0
-	BurnToHP = GetMaxHP("")-(GetMaxHP("")*(PercentDamage/100))
-	if BurnToHP < (GetMaxHP("")*0.2) then
-		BurnToHP = -25
-	end
-	SetProperty("Owner", "BurnToHP", BurnToHP)
-	SetProperty("Owner", "BurningToDo", PercentDamage)
+	
+	-- calculate the actual damage
+	local BaseFireDmg = RandomFire * MaxHP
+	local ActualFireDmg = math.ceil(BaseFireDmg * (1 - FireProt)) -- each % of FireProt reduces the dmg
 
-	CommitAction("fire","Owner","Owner")
+	CommitAction("fire", "Owner", "Owner")
 	
 	-- prevents worker's dwellings from being destroyed
 	
-	local BurnToHP = 0
-	BurnToHP = GetMaxHP("")-(GetMaxHP("")*(PercentDamage/100))
-	if BuildingGetType("")==1 then
-		if BurnToHP < (GetMaxHP("")*0.2) then	
-			BurnToHP = (GetMaxHP("")*0.2)
+	if BuildingGetType("") == GL_BUILDING_TYPE_WORKER_HOUSING then
+		if BurnToHP < (GetMaxHP("") * 0.1) then	
+			BurnToHP = (GetMaxHP("") * 0.1)
 		end
 	end 
 
@@ -37,7 +57,7 @@ function Run()
 		FireLocatorCount = FireLocatorCount + 1
 	end
 
-	if FireLocatorCount>0 then
+	if FireLocatorCount > 0 then
 		FireLocatorCount = FireLocatorCount
 	end
 
@@ -62,83 +82,74 @@ function Run()
 			GfxStartParticle("Spark"..SparkCount, "particles/spark1.nif", "FlameLocator"..SparkCount, 8)
 		end
 	end
-
-	Attach3DSound("", "fire/Fire_01.wav", 1,0)
 	
-	local DPS = (FireFactor/150)*GetMaxHP("")
+	if Rand(2) == 0 then
+		Attach3DSound("", "fire/Fire_01.wav", 1.0)
+	else
+		Attach3DSound("", "fire/Fire_l_02.wav", 1.0)
+	end
+	
+	local DPS = math.ceil(ActualFireDmg / 15)
+	if DPS < 20 then
+		DPS = 20
+	end
+	local DmgDealt = 0
 
-	while GetHP("Owner")>BurnToHP do
-
+	while DmgDealt < ActualFireDmg do
+		
 		Evacuate("Owner")
-		Sleep(3)
-		Attach3DSound("", "fire/Fire_01.wav", 1,0)
-
-		PercentDamage = GetProperty("Owner", "BurningToDo")
-		if not PercentDamage or PercentDamage<1 then
-			break
+		Sleep(5)
+		PlaySound3D("", "fire/DartingFlame_s_02.wav", 1.0)
+		
+		if BuildingGetType("Owner") == GL_BUILDING_TYPE_WORKER_HOUSING then
+			if GetHPRelative("Owner") >= 0.1 then
+				ModifyHP("Owner", -DPS, false)
+			end
+		else
+			ModifyHP("Owner", -DPS, false)
 		end
-		if (Weather_GetSeason()==3) then
-			DPS = DPS * 0.75
-		end
-		ModifyHP("Owner", -DPS, false)
-		--TFTODO: RAIN DAY
-		BurnToHP = GetProperty("Owner","BurnToHP")
-		local RainValue = Weather_GetValue(0)
-		if RainValue > 0 then
-			BurnToHP = GetProperty("Owner","BurnToHP")
-			local NewValue = 0+BurnToHP+(RainValue*(0.03*GetMaxHP("Owner")))
-			SetProperty("Owner","BurnToHP",NewValue )
-		end
-
-
-		PercentDamage = PercentDamage - FireFactor
-		SetProperty("Owner", "BurningToDo", PercentDamage)
+		
+		DmgDealt = DmgDealt + DPS
+		
+		Sleep(5)
+		-- fire may spread
 		local BuildingFilter = "__F((Object.GetObjectsByRadius(Building) == 1500)AND NOT(Object.GetState(burning))AND NOT(Object.HasImpact(Extinguished))AND NOT(Object.IsClass(5))AND NOT(Object.IsClass(3))AND NOT(Object.IsClass(6))AND NOT(Object.IsClass(0)))"
-		local NumBuildings = Find("",BuildingFilter,"NexBuilding", -1)
-		local FirePos = 95
-		local ExtPos = 95
-		if (Weather_GetSeason()==1) then -- higher chance for fire going to next building in summer
-			FirePos = FirePos - 5
-			ExtPos = ExtPos + 2 -- lesser chance for fire getting extinguished by rain in summer
-		elseif (Weather_GetSeason()==3) then -- lower chance for fire going to next building in winter
-			FirePos = FirePos + 2
-			ExtPos = ExtPos - 2 -- higher chance for fire getting extinguished by snow in summer
-		elseif (Weather_GetSeason()==2) then -- slightly lower chance for fire going to next building in winter
-			FirePos = FirePos + 1
-			ExtPos = ExtPos - 5 -- higher chance for fire getting extinguished by rain in fall
+		local NumBuildings = Find("", BuildingFilter, "NexBuilding", -1)
+		local Chance = 80
+		
+		if (Weather_GetSeason() == 1) then -- higher chance for fire going to next building in summer
+			Chance = Chance + 10
+		elseif (Weather_GetSeason() == 3) then -- lower chance for fire going to next building in winter
+			Chance = Chance - 5
 		end
 
 		if (Weather_GetValue(0) > 0.5) then -- weather value 0 is precipitation
-			FirePos = FirePos + 2 -- lesser chance for fire going to next building when rain or snow
-			if (Rand(100) > ExtPos) then
-				break
-			end
+			Chance = Chance - 10 -- lesser chance for fire going to next building when rain or snow
 		end
 		
 		if NumBuildings > 0 then
-			if Rand(100) > FirePos then
+			if Chance > Rand(100) then
 				local DestAlias = "NexBuilding"..Rand(NumBuildings-1)
-				local FireProtection = GetImpactValue(DestAlias,"ProtectionFromFire")*100
-				if (Rand(100) >= FireProtection) then
-					if GetPosition("","ParticleSpawnPos") then
-						StartSingleShotParticle("particles/Explosion.nif", "ParticleSpawnPos", 4,5)
-						PlaySound3D(DestAlias,"fire/Explosion_s_01.wav", 1.0)
+				local FireProtection = GetImpactValue(DestAlias, "ProtectionFromFire")*100
+				if (Rand(100) > FireProtection) then
+					if GetPosition("", "ParticleSpawnPos") then
+						StartSingleShotParticle("particles/Explosion.nif", "ParticleSpawnPos", 4, 5)
+						PlaySound3D(DestAlias, "fire/Explosion_s_01.wav", 1.0)
 						Sleep(2)
 					end
-					SetState(DestAlias,STATE_BURNING,true)
+					SetState(DestAlias, STATE_BURNING, true)
 				end
 			end
 		end
-		AddImpact("","Extinguished",1,4)
+		
+		Sleep(5)
 	end
-
-	SetState("OWNER", STATE_BURNING, false)
-
+	
+	AddImpact("Owner", "Extinguished", 1, 8)
+	SetState("Owner", STATE_BURNING, false)
 end
 
 function CleanUp()
 	Detach3DSound("")
-	StopAction("fire","Owner")
-	RemoveProperty("Owner", "BurningToDo")
-	RemoveProperty("Owner", "BurnToHP")
+	StopAction("fire", "Owner")
 end
