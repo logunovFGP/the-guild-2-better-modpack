03656e15d57a7c5291a0a639c746fca69714bdea
Fajeth <thefajeth@gmail.com>
2023-04-11 15:27:27 +0200

    Fixed embezzle public money gain

--- Scripts/Measures/ms_108_EmbezzlePublicMoney.lua
+++ Scripts/Measures/ms_108_EmbezzlePublicMoney.lua
@@ -1,4 +1,5 @@
 function Run()
+
 	if not ai_GoInsideBuilding("", "", -1, GL_BUILDING_TYPE_TOWNHALL) then
 		return
 	end
@@ -15,28 +16,35 @@ function Run()
 	local money = 0
 	local MaxMoney = (CityTreasure - CityTreasureMin)/10
 	
+	if MaxMoney > 10000 then
+		MaxMoney = 10000
+	end
+	
+	local MinSteal = MaxMoney * 0.25
+	
 	if CityTreasure < CityTreasureMin then
 		MsgQuick("", "@L_MEASURE_EMBEZZLEPUBLICMONEY_FAILURE_+0", GetID("city"))
 		return
-	end
-	
-	if MaxMoney > 5000 then
-		MaxMoney = 5000
+	else
+		local Random = Rand(10) + 1
+		money = ((MaxMoney - MinSteal) / 10) * Random + MinSteal
 	end
 	
 	GetInsideBuilding("", "Townhall")
+	
 	if GetLocatorByName("Townhall", "EmbezzleMoney", "Result") then
 		f_MoveTo("", "Result")
 	end
+	
 	PlayAnimation("", "manipulate_middle_twohand")
 	
 	SetMeasureRepeat(TimeOut)
 	chr_SpendMoney("city", money, "CostThiefs")
-	CreditMoney("", money, "EmbezzledPublicMoney")
+	CreditMoney("", money, "IncomeThiefs")
 	
 	feedback_MessageCharacter("Owner",
-		"@L_PRIVILEGES_108_EMBEZZLEPUBLICMONEY_SUCCESS_HEAD_+0",
-		"@L_PRIVILEGES_108_EMBEZZLEPUBLICMONEY_SUCCESS_BODY_+0", GetID("Owner"), GetID("city"), money)
+						"@L_PRIVILEGES_108_EMBEZZLEPUBLICMONEY_SUCCESS_HEAD_+0",
+						"@L_PRIVILEGES_108_EMBEZZLEPUBLICMONEY_SUCCESS_BODY_+0", GetID("Owner"), GetID("city"), money)
 
 	chr_GainXP("", GetData("BaseXP"))
 
@@ -46,6 +54,6 @@ end
 
 function GetOSHData(MeasureID)
 	--can be used again in:
-	OSHSetMeasureRepeat("@L_ONSCREENHELP_7_MEASURES_TIMEINFOS_+2",Gametime2Total(mdata_GetTimeOut(MeasureID)))
+	OSHSetMeasureRepeat("@L_ONSCREENHELP_7_MEASURES_TIMEINFOS_+2", Gametime2Total(mdata_GetTimeOut(MeasureID)))
 end
 
