39c8ccad6b9346546e06839394daf0af22b5f7ea
Fajeth <thefajeth@gmail.com>
2023-11-05 17:46:17 +0100

    Alias-fix levelup city

--- Scripts/Measures/ms_LevelUpCity.lua
+++ Scripts/Measures/ms_LevelUpCity.lua
@@ -8,8 +8,8 @@ function Run()
 
 	local MeasureID = GetCurrentMeasureID("")
 	local TimeOut = mdata_GetTimeOut(MeasureID)
-	local Level = CityGetLevel("city")
-	local CityTreasure = GetMoney("city")
+	local Level = CityGetLevel("MyCity")
+	local CityTreasure = GetMoney("MyCity")
 	local CityUpgradeCost = gameplayformulas_GetCityUpgradeCost(Level)
 	local citylabel = CityLevel2Label(Level)
 	local nomorelvlup = false
@@ -27,9 +27,9 @@ function Run()
 	if nomorelvlup==true then
 		MsgBoxNoWait("",false,
 			"@L_MEASURE_LEVELUPCITY_HEAD_+0",
-			"@L_MEASURE_LEVELUPCITY_BODY_+5", GetID("city"), citylabel)
+			"@L_MEASURE_LEVELUPCITY_BODY_+5", GetID("MyCity"), citylabel)
 			
-	elseif HasProperty("city", "LevelUpPaid") and GetProperty("city","LevelUpPaid") == 1 then
+	elseif HasProperty("MyCity", "LevelUpPaid") and GetProperty("MyCity","LevelUpPaid") == 1 then
 		GetScenario("scenario")
 		local mapid = GetProperty("scenario", "mapid")
 		local scenarioname = GetDatabaseValue("maps", mapid, "lordship")
@@ -37,13 +37,13 @@ function Run()
 		local lordlabel = "@L_SCENARIO_LORD_"..GetDatabaseValue("maps", mapid, "lordship").."_+1"
 		MsgBoxNoWait("", false,
 			"@L_MEASURE_LEVELUPCITY_HEAD_+0",
-			"@L_MEASURE_LEVELUPCITY_BODY_+4", GetID("city"), citylabel, lordlabel)
+			"@L_MEASURE_LEVELUPCITY_BODY_+4", GetID("MyCity"), citylabel, lordlabel)
 	
-	elseif HasProperty("city","LevelUpCity") and GetProperty("city","LevelUpCity")==1 then
-		if CityTreasure<CityUpgradeCost then
-			MsgBoxNoWait("",false,
+	elseif HasProperty("MyCity", "LevelUpCity") and GetProperty("MyCity", "LevelUpCity") == 1 then
+		if CityTreasure < CityUpgradeCost then
+			MsgBoxNoWait("", false,
 				"@L_MEASURE_LEVELUPCITY_HEAD_+0",
-				"@L_MEASURE_LEVELUPCITY_BODY_+0", GetID("city"), CityUpgradeCost, citylabel)
+				"@L_MEASURE_LEVELUPCITY_BODY_+0", GetID("MyCity"), CityUpgradeCost, citylabel)
 		else
 			local Result = MsgNews("",false,"@P"..
 						"@B[1,@L_REPLACEMENTS_BUTTONS_JA_+0]"..
@@ -52,49 +52,49 @@ function Run()
 						"default", --MessageClass
 						2, --TimeOut
 						"@L_MEASURE_LEVELUPCITY_HEAD_+0",
-						"@L_MEASURE_LEVELUPCITY_BODY_+1", GetID("city"), CityUpgradeCost, citylabel)
+						"@L_MEASURE_LEVELUPCITY_BODY_+1", GetID("MyCity"), CityUpgradeCost, citylabel)
 
-			if Result==1 then
+			if Result == 1 then
 				local	ImperialId = ScenarioGetImperialCapitalId()
-				if Level==5 then
-					if (ImperialId<1 or ImperialId==nil) then
-						ScenarioSetImperialCapital("city")
-						SetProperty("city","ImperialCapital",1)
-						SetProperty("city","LevelUpPaid",1)
-						chr_SpendMoney("city",CityUpgradeCost,"LevelUpPaid")
+				if Level == 5 then
+					if (ImperialId<1 or ImperialId == nil) then
+						ScenarioSetImperialCapital("MyCity")
+						SetProperty("MyCity", "ImperialCapital", 1)
+						SetProperty("MyCity", "LevelUpPaid", 1)
+						chr_SpendMoney("MyCity", CityUpgradeCost, "LevelUpPaid")
 						SetMeasureRepeat(TimeOut)
 					end
 				else
-					SetProperty("city","LevelUpPaid",1)
-					chr_SpendMoney("city",CityUpgradeCost,"LevelUpPaid")
+					SetProperty("MyCity", "LevelUpPaid", 1)
+					chr_SpendMoney("MyCity", CityUpgradeCost, "LevelUpPaid")
 					SetMeasureRepeat(TimeOut)
 				end
 			end
 		end
 	
 	else
-		if CityTreasure<CityUpgradeCost then
-			MsgBoxNoWait("",false,
+		if CityTreasure < CityUpgradeCost then
+			MsgBoxNoWait("", false,
 				"@L_MEASURE_LEVELUPCITY_HEAD_+0",
-				"@L_MEASURE_LEVELUPCITY_BODY_+2", GetID("city"), CityUpgradeCost, citylabel, citizens)
+				"@L_MEASURE_LEVELUPCITY_BODY_+2", GetID("MyCity"), CityUpgradeCost, citylabel, citizens)
 		else
-			MsgBoxNoWait("",false,
+			MsgBoxNoWait("", false,
 				"@L_MEASURE_LEVELUPCITY_HEAD_+0",
-				"@L_MEASURE_LEVELUPCITY_BODY_+3", GetID("city"), citylabel, citizens)
+				"@L_MEASURE_LEVELUPCITY_BODY_+3", GetID("MyCity"), citylabel, citizens)
 		end
 	end
 end
 
 function GetValue(Level)
-	if Level==2 then
+	if Level == 2 then
 		return {0, 80}
-	elseif Level==3 then
+	elseif Level == 3 then
 		return {90, 130}
-	elseif Level==4 then
+	elseif Level == 4 then
 		return {140, 200}
-	elseif Level==5 then
+	elseif Level == 5 then
 		return {200, 300}
-	elseif Level==6 then
+	elseif Level == 6 then
 		return {260, 99999}
 	end
 end
@@ -105,6 +105,6 @@ end
 
 function GetOSHData(MeasureID)
 	--can be used again in:
-	OSHSetMeasureRepeat("@L_ONSCREENHELP_7_MEASURES_TIMEINFOS_+2",Gametime2Total(mdata_GetTimeOut(MeasureID)))
+	OSHSetMeasureRepeat("@L_ONSCREENHELP_7_MEASURES_TIMEINFOS_+2", Gametime2Total(mdata_GetTimeOut(MeasureID)))
 end
 
