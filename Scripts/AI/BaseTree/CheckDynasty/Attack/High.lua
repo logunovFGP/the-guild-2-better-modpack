7f0f183b172e9ab80c017aef5ab8234f20f586cf
Fajeth <thefajeth@gmail.com>
2024-07-08 21:21:58 +0200

    fixed TWP_AI_Merge error

--- Scripts/AI/BaseTree/CheckDynasty/Attack/High.lua
+++ Scripts/AI/BaseTree/CheckDynasty/Attack/High.lua
@@ -1,17 +1,10 @@
 function Weight()
-<<<<<<< master
-	if ai_AICheckAction() == false then
-		return 0
-	end
 
-	local Favor = GetFavorToSim("Victim", "dynasty")
-=======
 	if ai_AICheckAction()==false then
 		return 0
 	end
 
 	local Favor = GetFavorToSim("dynasty", "Victim")
->>>>>>> e6fb8ee first try at importing the TWP AI
 	local Difficulty = ScenarioGetDifficulty()
 	local RetVal = 40 + (Difficulty * 15) - Favor
 	if RetVal<0 then
