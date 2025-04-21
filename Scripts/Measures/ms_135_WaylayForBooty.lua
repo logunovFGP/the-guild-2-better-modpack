66ed0d6b758d6788bae850c0500145b59e9c532e
naonauno <desk.nao@gmail.com>
2025-04-20 20:05:56 -0700

    Major Squad hotfix
    Fixed a critical issue with consistent item duplicating with Squads Waylay

--- Scripts/Measures/ms_135_WaylayForBooty.lua
+++ Scripts/Measures/ms_135_WaylayForBooty.lua
@@ -3,20 +3,22 @@ function Run()
 		return
 	end
 	
-	local	Distance = Rand(400)+100
+	local Distance = Rand(400) + 100
 	if not f_MoveTo("", "Destination", GL_MOVESPEED_RUN, Distance) then
 		return
 	end
 
-	local BootyFilter	= "__F((Object.GetObjectsByRadius(Sim)== 1500) AND(Object.BelongsToMe())AND(Object.GetProfession()==15))"
-	local NumRobbers = Find("Destination", BootyFilter, "Robbers", -1)
+	local BootyFilter = "__F((Object.GetObjectsByRadius(Sim)== 1500) AND(Object.BelongsToMe())AND(Object.GetProfession()==15))"
+	local NumRobbers  = Find("Destination", BootyFilter, "Robbers", -1)
 	
-	for i=0, NumRobbers-1 do
+	for i = 0, NumRobbers - 1 do
 		if SquadGet("Robbers"..i, "Squad") then
+			LogMessage("@WAYLAY ".. GetName("") .. " has been added to " .. GetName("Robbers"..i) .. "'s squad.")
 			SquadAddMember("Squad", -1, "")
 			return
 		end
 	end
-	
+
+	LogMessage("@WAYLAY ".. GetName("") .. " has created a new squad.")
 	SquadCreate("", "SquadWaylayForBooty", "Destination", "SquadWaylayMember", "SquadWaylayMember")
 end
